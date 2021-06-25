b3008691bdd4f6e03e5252739d8565a0b7161b02
janaestewart <janae.stewart94@gmail.com>
2021-06-25 12:42:59 -0400

    vowelCounter

---
+++ vowelCounter.js
@@ -0,0 +1,23 @@
+/* CHALLENGE
+Given a string of text, return the number of vowels found within the text
+e.g vowelsCounter('anehizxcv') // will return 3
+*/
+
+//pseudoCode breakdown
+    //We write a function that’d receive a parameter called text. It would be a string of any length which would be passed to the function as an argument when it is called.
+
+    // Next, within the function we have to go through the text and search for occurrences of the English vowels (a,e,i,o,u).
+
+    // The function then returns the total number of matches(vowels) found. This brings return statements to mind as they simply stop the execution of a function and return a value from that function.
+
+//2 ways of solving this
+
+    //an iterative approach
+        //
+
+    //using regular expressions
+
+
+function vowelsCounter(text) {
+    // Code goes here
+}
