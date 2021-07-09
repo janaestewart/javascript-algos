2d5a94ec031b4226caf2e45b26797b912ca2b1a2
janaestewart <janae.stewart94@gmail.com>
2021-07-09 08:48:38 -0400

    beginning algo

---
+++ findMostRecurringChar.js
@@ -0,0 +1,10 @@
+//Finding the most recurring character
+
+//Dealing with strings, arrays and objects
+
+
+//THE CHALLENGE
+// Given a string of text, find and return the most recurring character: 
+    maxRecurringChar('aabacada') // will return 'a'
+
+//Parameter: the string of text
